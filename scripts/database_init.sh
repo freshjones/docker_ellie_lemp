0d607a2e7f6afa1c3dfb6dca548141351563fc1d
William Jones <billy@freshjones.com>
2015-02-02 14:13:43 -0500

    another atempt

--- scripts/database_init.sh
+++ scripts/database_init.sh
@@ -6,8 +6,8 @@ if [[ ! -d $VOLUME_HOME/mysql ]]; then
     echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
     echo "=> Installing MySQL ..."
     mysql_install_db > /dev/null 2>&1
-    echo "=> Done!"  
-    /scripts/database_install.sh
 else
     echo "=> Using an existing volume of MySQL"
 fi
+
+/scripts/database_install.sh
