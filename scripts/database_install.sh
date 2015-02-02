de48bdc7ce750bcf6315c5f9805fe988463fd56c
William Jones <billy@freshjones.com>
2015-02-02 14:46:18 -0500

    another attempt

--- scripts/database_install.sh
+++ scripts/database_install.sh
@@ -14,7 +14,7 @@ mysql -uroot -pwelcome -e "CREATE DATABASE elliesite;"
 mysql -uroot -pwelcome -e "GRANT ALL PRIVILEGES ON elliesite.* TO 'admin'@'localhost' IDENTIFIED BY 'welcome'; FLUSH PRIVILEGES;"
 
 echo "cd into app"
-cd /app/laravel
+cd /app
 
 echo "run migration script"
 php artisan migrate
